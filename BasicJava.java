3beeb82c2ec4462919f603f3e495eca52e6ec9a6
Snehal Patil <141567039+snehal112211@users.noreply.github.com>
2023-08-21 01:05:03 +0530

    Add files via upload

---
+++ BasicJava.java
@@ -0,0 +1,9 @@
+// package in java is used to group related classes.
+// Think of it as a folder in the file directory
+
+public class BasicJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+    
+}
