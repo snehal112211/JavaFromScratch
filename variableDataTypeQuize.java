90fe8ea4ec8e56dee7bc5aededd09865232e9fe1
Snehal Patil <141567039+snehal112211@users.noreply.github.com>
2023-08-22 01:35:04 +0530

    Add files via upload

---
+++ variableDataTypeQuize.java
@@ -0,0 +1,14 @@
+// write the java program to add the three number.
+
+public class variableDataTypeQuize{
+    public static void main(String[] args){
+        System.out.println("The sum of three number is: ");
+        int num1 = 12;
+        int num2 = 45;
+        int num3 = 34;
+        int sum = num1+ num2+ num3;
+        System.out.println(sum);
+
+    }
+
+}
